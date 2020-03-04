274895dbaf1185634f9e149cc8c6377bf3cbf6ae
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2020-03-04 14:33:10 +0100

    Creación mediante fichero

--- src/main/java/plugin.java
+++ src/main/java/plugin.java
@@ -7,14 +7,13 @@ import java.io.IOException;
 
 public class plugin {
     public static void main(String[] args) throws IOException {
-        String token = JOptionPane.showInputDialog(null, "Teclea tu token");
+        String nome = JOptionPane.showInputDialog("Nombre del repositorio");
+        String descripcion = JOptionPane.showInputDialog("Descripcion");
 
-        GitHub github = new GitHubBuilder().withOAuthToken(token).build();
+        GitHub github = GitHubBuilder.fromPropertyFile("").build();
 
-        GHRepository repo = github.createRepository(
-                "PluginTresRamasToken","Tiene tres ramas",
-                "https://github.com/jreyarijon",false/*private*/);
-        repo.addCollaborators(github.getUser("damianCastelao"));
+        GHRepository repo = github.createRepository(nome,descripcion,
+                "https://github.com/jreyarijon",false);
 
     }
 
